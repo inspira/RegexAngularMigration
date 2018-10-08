62175e1934a8de163595fd291b45fce714195c55
Rogério Teixeira <rogerio.teixeira@inspira.com.br>
2018-10-08 17:18:45 -0300

    Adicionado arquivos de apoio para validar arquivos refatorados com jshint e eslint

---
+++ SupportFiles/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": false
+    },
+    "plugins": [
+		"angular",
+	],
+    "parserOptions": {
+        "ecmaVersion": 2015
+    },
+    "rules": {
+		"angular/no-http-callback": 2
+    }
+};
